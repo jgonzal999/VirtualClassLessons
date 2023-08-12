25aaa31941a373e4e0d7683ec9d3d0bc46cbc3ce
Javier <jgonzal99@gmail.com>
2023-08-12 10:50:11 +0000

    Lesson4-6

--- Lesson4/Lesson4Exercises/src/exercise5/LinkedList.java
+++ Lesson4/Lesson4Exercises/src/exercise5/LinkedList.java
@@ -71,10 +71,10 @@ public class LinkedList {
 		// Insert the values 
 		list = insert(list, 1); 
 		list = insert(list, 2); 
-		list = insert(list, 3); 
+		list = insert(list, 4); 
 		list = insert(list, 4); 
 		list = insert(list, 5); 
-		list = insert(list, 6); 
+		list = insert(list, 1); 
 		list = insert(list, 7); 
 		list = insert(list, 8); 
 		// Print the LinkedList 
---
+++ Lesson4/Lesson4Exercises/src/increasing/LongestIncreasingSubs.java
@@ -0,0 +1,18 @@
+package increasing;
+
+public class LongestIncreasingSubs {
+	int rannumbers[];
+	int size;
+	LongestIncreasingSubs(){
+		for (int i=0;i<size;i++)
+			rannumbers[i]=(int)(Math.random()) * 100;
+			
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
