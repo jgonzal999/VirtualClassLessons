91a54f46a6f32a5a120b67c0a3db4de8e64f5c27
Javier <jgonzal99@gmail.com>
2023-08-18 10:36:57 +0000

    Lesson5-EX4

--- Lesson5/src/exercise4/SelectionSort.java
+++ Lesson5/src/exercise4/SelectionSort.java
@@ -1,10 +1,40 @@
 package exercise4;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class SelectionSort {
+	static int size=30;
+	static int value=15;
+	
+	public static int[] createnumbers(){
+		int rannumbers[] = new int[size];
+		for (int i=0;i<size;i++) {
+			rannumbers[i]=(int)(Math.random()*value);
+		}
+		return rannumbers;
+	}
+	
+	public static void selectionSort(int[] arr) {
+		
+		for(int i=0;i<arr.length-1;i++) {
+			int index =i;
+			for(int j=i+1;j<arr.length;j++) {
+				if(arr[j]<arr[index]) {
+				index=j;
+				}
+			}
+			int small=arr[index];
+			arr[index]=arr[i];
+			arr[i]=small;
+		}
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int[] randomlist =createnumbers();
+		System.out.println("Random List Elements: "+Arrays.toString(randomlist));
+		selectionSort(randomlist);
+		System.out.println("Sorted List Elements: "+Arrays.toString(randomlist));
 	}
 
 }
